b13da7cf8c63b82c2e10a0e648dcc034bf60d9be
Andrew McElvey <andrew.mcelvey@gmail.com>
2026-02-13 00:11:10 -0600

    Update greeting message in app.mjs

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World: <a href="https://github.com/andrewmcelvey2/turbo-carnival" target="blank">turbo carnival</a>')
+  res.send('Hello World: <a href="https://github.com/andrewmcelvey2/turbo-carnival" target="blank">turbo carnival</a> | dev')
 })
 
 app.listen(3000, () => {
